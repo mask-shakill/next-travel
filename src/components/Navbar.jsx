1834f78b6b2e1bede3e4c0cf63fd52f965da424d
mask-shakil <shakil.cse201@gmail.com>
2023-12-04 10:34:59 +0600

    change navbar color and json data

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <>
-      <nav className=" bg-nav-color text-black flex items-center justify-evenly py-3">
+      <nav className=" bg-slate-300 text-black flex items-center justify-evenly py-3">
         <div className="flex items-center ">
           <img
             src="https://cdn-icons-png.flaticon.com/128/9502/9502563.png?ga=GA1.1.945868740.1697346172&semt=ais"
@@ -13,12 +13,12 @@ const Navbar = () => {
             alt=""
           />
 
-          <Link className="mx-5" href={"/destinations"}>
+          <Link className="mx-5 font-semibold" href={"/destinations"}>
             Destinations
           </Link>
         </div>
 
-        <ul className="flex items-center gap-7">
+        <ul className="flex items-center font-semibold gap-7">
           <Link href={"/"}>Home</Link>
           <Link href={"/most-favorite"}>Most Favorite</Link>
 
@@ -26,7 +26,7 @@ const Navbar = () => {
 
           <Link href={"/about"}>About</Link>
 
-          <Link href={"/contact"}>contact</Link>
+          <Link href={"/contact"}>Contact</Link>
         </ul>
       </nav>
     </>
